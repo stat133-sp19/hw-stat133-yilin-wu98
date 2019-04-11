10870cf8f6d8fc775b6f37586931a9e6c7dc772d
yilin-wu98 <yilin-wu@outlook.com>
2019-04-11 16:58:11 -0700

    workout updated

--- workout02/app.R
+++ workout02/app.R
@@ -11,51 +11,51 @@ library(shiny)
 library(ggplot2)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-          titlePanel("Investment Profit app"),
-          
-          fluidRow(
-            column(4,
-                   
-                     sliderInput("initial_amount",
-                                 "Initial Amount",
-                                 min = 1,
-                                 max = 100000,
-                                 step= 10000,
-                                 pre = "$", sep = ",",
-                                 value = 1000
-                                 ),
-                     sliderInput('annual_contrib',
-                                 'Annual Contribution',
-                                 min=0,
-                                 max=50000,
-                                 pre='$',sep=',',
-                                 value=2000)
-            ),
-            column(4,
-                   sliderInput('return_rate','
-                    Return Rate (in%)',
-                               min=0,max=100,
-                               step=2,value=5),
-                   sliderInput('growth_rate','Growth Rate (in%)',
-                               min=0,max=100,step=2,value=2)
-            ),
-            column(4,
-                   sliderInput('year',
-                  'Years',
-                               min=0,max=50,
-                               step=1,value=10),
-                   selectInput('facet','Facet',
-                               c('No','Yes'))
-            ),
-            column(12,textOutput(outputId = "title"
-                                )),
-            column(12,plotOutput("plot")),
-            column(12,textOutput(outputId='table')),
-            column(12,tableOutput('balances'))
-            
-          
-            )
-)
+  titlePanel("Investment Profit app"),
+  
+  fluidRow(
+    column(4,
+           
+           sliderInput("initial_amount",
+                       "Initial Amount",
+                       min = 1,
+                       max = 100000,
+                       step= 10000,
+                       pre = "$", sep = ",",
+                       value = 1000
+           ),
+           sliderInput('annual_contrib',
+                       'Annual Contribution',
+                       min=0,
+                       max=50000,
+                       pre='$',sep=',',
+                       value=2000)
+    ),
+    column(4,
+           sliderInput('return_rate','
+                       Return Rate (in%)',
+                       min=0,max=100,
+                       step=2,value=5),
+           sliderInput('growth_rate','Growth Rate (in%)',
+                       min=0,max=100,step=2,value=2)
+    ),
+    column(4,
+           sliderInput('year',
+                       'Years',
+                       min=0,max=50,
+                       step=1,value=10),
+           selectInput('facet','Facet',
+                       c('No','Yes'))
+    ),
+    column(12,textOutput(outputId = "title"
+    )),
+    column(12,plotOutput("plot")),
+    column(12,textOutput(outputId='table')),
+    column(12,tableOutput('balances'))
+    
+    
+  )
+  )
 
 
 server <- function(input, output) {
@@ -121,23 +121,25 @@ server <- function(input, output) {
   }
   
   #TWO reactive dataframe mode_i and mode_i_full
-  mode_i<- reactive( { for (i in 0:input$year){
-    mode[i+1,'year'] <- i
-    mode[i+1,'no_contrib'] <- future_value(amount=input$initial_amount,rate=input$return_rate/100,years=i)
-    mode[i+1,'fixed_contrib']<-future_value(amount=input$initial_amount,rate=input$return_rate/100,years=i)+annuity(contrib=input$annual_contrib,rate=input$return_rate/100,years=i)
-    mode[i+1,'growing_contrib']<-future_value(amount=input$initial_amount,rate=input$return_rate/100,years=i)+growing_annuity(contrib=input$annual_contrib,rate=input$return_rate/100,growth=input$growth_rate/100,years=i)
-  }
-    mode})
+  mode_in<- reactive({ 
+    mode_i <-data.frame()
+    for (i in 0:input$year){
+      mode_i[i+1,'year'] <- i
+      mode_i[i+1,'no_contrib'] <- future_value(amount=input$initial_amount,rate=input$return_rate/100,years=i)
+      mode_i[i+1,'fixed_contrib']<-future_value(amount=input$initial_amount,rate=input$return_rate/100,years=i)+annuity(contrib=input$annual_contrib,rate=input$return_rate/100,years=i)
+      mode_i[i+1,'growing_contrib']<-future_value(amount=input$initial_amount,rate=input$return_rate/100,years=i)+growing_annuity(contrib=input$annual_contrib,rate=input$return_rate/100,growth=input$growth_rate/100,years=i)
+    }
+    mode_i})
   
   mode_i_full <- reactive({ mode_1 <- data.frame()
-  mode_1[1:(input$year+1),'value']<-mode_i()[1:(input$year+1),'no_contrib']
-  mode_1[1:(input$year+1),'year']<-mode_i()[1:(input$year+1),'year']
+  mode_1[1:(input$year+1),'value']<-mode_in()[1:(input$year+1),'no_contrib']
+  mode_1[1:(input$year+1),'year']<-mode_in()[1:(input$year+1),'year']
   mode_1[1:(input$year+1),'variable']<-'no_contrib'
-  mode_1[(input$year+2):(2*input$year+2),'year']<-mode_i()[1:(input$year+1),'year']
-  mode_1[(input$year+2):(2*input$year+2),'value']<-mode_i()[1:(input$year+1),'fixed_contrib']
+  mode_1[(input$year+2):(2*input$year+2),'year']<-mode_in()[1:(input$year+1),'year']
+  mode_1[(input$year+2):(2*input$year+2),'value']<-mode_in()[1:(input$year+1),'fixed_contrib']
   mode_1[(input$year+2):(2*input$year+2),'variable']<-'fixed_contrib'
-  mode_1[(2*input$year+3):(3*input$year+3),'year']<-mode_i()[1:(input$year+1),'year']
-  mode_1[(2*input$year+3):(3*input$year+3),'value']<-mode_i()[1:(input$year+1),'growing_contrib']
+  mode_1[(2*input$year+3):(3*input$year+3),'year']<-mode_in()[1:(input$year+1),'year']
+  mode_1[(2*input$year+3):(3*input$year+3),'value']<-mode_in()[1:(input$year+1),'growing_contrib']
   mode_1[(2*input$year+3):(3*input$year+3),'variable']<-'growing_contrib'
   mode_1[,'variable']<-factor(mode_1[,'variable'],levels=c('no_contrib','fixed_contrib','growing_contrib'),label=c('no_contrib','fixed_contrib','growing_contrib'))
   mode_1})
@@ -148,47 +150,49 @@ server <- function(input, output) {
   #output a plot
   output$plot <- renderPlot({
     if (input$facet=='No'){
-    ggplot(mode_i())+
-      geom_line(aes(x=year,y=no_contrib,color='red'))+
-      geom_point(aes(x=year,y=no_contrib,color='red'))+
-      geom_line(aes(x=year,y=fixed_contrib,color='green'))+
-      geom_point(aes(x=year,y=fixed_contrib,color='green'))+
-      geom_line(aes(x=year,y=growing_contrib,color='blue'))+
-      geom_point(aes(x=year,y=growing_contrib,color='blue'))+
-      scale_color_identity(labels=factor(c('growing_contrib','fixed_contrib','no_contrib')
-      ),guide='legend',name='mode')+
-      theme(legend.text=element_text(size=rel(1.2)))+
-      scale_x_continuous(breaks=seq(0.0,input$year,by=2.5),limits=c(0,input$year))+
-      xlab("year")+
-      ylab("value")+
-      ggtitle("Three modes of investing")
+      
+      ggplot(mode_in())+
+        geom_line(aes(x=year,y=no_contrib,color='red'))+
+        geom_point(aes(x=year,y=no_contrib,color='red'))+
+        geom_line(aes(x=year,y=fixed_contrib,color='green'))+
+        geom_point(aes(x=year,y=fixed_contrib,color='green'))+
+        geom_line(aes(x=year,y=growing_contrib,color='blue'))+
+        geom_point(aes(x=year,y=growing_contrib,color='blue'))+
+        scale_color_identity(labels=factor(c('growing_contrib','fixed_contrib','no_contrib')
+        ),guide='legend',name='mode_i')+
+        theme(legend.text=element_text(size=rel(1.2)))+
+        scale_x_continuous(breaks=seq(0.0,input$year,by=2.5),limits=c(0,input$year))+
+        xlab("year")+
+        ylab("value")+
+        ggtitle("Three modes of investing")
     }else{
-     
+      
       ggplot(mode_i_full(),aes(color=variable))+
         geom_line(aes(x=year,y=value))+
         geom_point(aes(x=year,y=value),size=0.8)+
         geom_ribbon(data=mode_i_full(),aes(x=year,ymin=0,ymax=value,fill=variable),alpha=0.2)+
         facet_grid(.~variable)+
-        # scale_color_identity(labels=c('growing_contrib','no_contrib','fixed_contrib'),guide='legend',name='mode')+
+        # scale_color_identity(labels=c('growing_contrib','no_contrib','fixed_contrib'),guide='legend',name='mode_i')+
         theme(legend.text=element_text(size=rel(1.2)))+
         scale_x_continuous(breaks=seq(0,input$year,by=5),limits=c(0,input$year))+
         xlab("year")+
         ylab("value")+
         ggtitle("The modes of investing")
       
-      }
+    }
     
-
-  
+    
+    
   },height=400,width=900)
   
   #output a title
   output$table<-renderText('Balances')
   #output a table
   output$balances <- renderTable({
-    mode_i()
-
-
+    
+    mode_in()
+    
+    
   },height=200,width=900)
 }
 
